c9f5cce6205e73b92a0108621679c96ef233c249
kartik <kartikd@mindfiresolutions.com>
2020-02-19 12:08:14 +0530

    first commit

---
+++ demo/src/main/java/com/api/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+
+@SpringBootApplication(scanBasePackages = "com.api")
+@EnableJpaRepositories(basePackages  = "com.api.repository")
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+
+}
